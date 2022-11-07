046aaaba3c37cf659368c97ad92d5b0c5fa57314
augustoherrera99 <augustofherrera@hotmail.com>
2022-11-07 03:17:26 -0300

    Agregar archivos de proyecto.

---
+++ pryHerreraSP4_BarLaMilanga/frmBarLaMilanga.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace pryHerreraSP4_BarLaMilanga
+{
+    partial class frmBarLaMilanga
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmBarLaMilanga));
+            this.dgvGrillaVentas = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnValidarDatos = new System.Windows.Forms.Button();
+            this.btnMozoDelDia = new System.Windows.Forms.Button();
+            this.btnTotales = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.mrcConsultas = new System.Windows.Forms.GroupBox();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.lstResultado = new System.Windows.Forms.ListBox();
+            this.lblVentas = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvGrillaVentas)).BeginInit();
+            this.mrcConsultas.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvGrillaVentas
+            // 
+            this.dgvGrillaVentas.AllowUserToAddRows = false;
+            this.dgvGrillaVentas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvGrillaVentas.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.dgvGrillaVentas.Location = new System.Drawing.Point(13, 29);
+            this.dgvGrillaVentas.Name = "dgvGrillaVentas";
+            this.dgvGrillaVentas.RowHeadersVisible = false;
+            this.dgvGrillaVentas.Size = new System.Drawing.Size(563, 133);
+            this.dgvGrillaVentas.TabIndex = 0;
+            this.dgvGrillaVentas.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.dgvGrillaVentas_KeyPress);
+            this.dgvGrillaVentas.Validating += new System.ComponentModel.CancelEventHandler(this.dgvGrillaVentas_Validating);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Mozos";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Comidas";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Bebidas sin alcohol";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 130;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Bebidas con alcohol";
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 130;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Postres";
+            this.Column5.Name = "Column5";
+            // 
+            // btnValidarDatos
+            // 
+            this.btnValidarDatos.Location = new System.Drawing.Point(498, 185);
+            this.btnValidarDatos.Name = "btnValidarDatos";
+            this.btnValidarDatos.Size = new System.Drawing.Size(78, 36);
+            this.btnValidarDatos.TabIndex = 1;
+            this.btnValidarDatos.Text = "Validar datos";
+            this.btnValidarDatos.UseVisualStyleBackColor = true;
+            this.btnValidarDatos.Click += new System.EventHandler(this.btnValidarDatos_Click);
+            // 
+            // btnMozoDelDia
+            // 
+            this.btnMozoDelDia.Location = new System.Drawing.Point(44, 54);
+            this.btnMozoDelDia.Name = "btnMozoDelDia";
+            this.btnMozoDelDia.Size = new System.Drawing.Size(75, 40);
+            this.btnMozoDelDia.TabIndex = 2;
+            this.btnMozoDelDia.Text = "Mozo del Día";
+            this.btnMozoDelDia.UseVisualStyleBackColor = true;
+            this.btnMozoDelDia.Click += new System.EventHandler(this.btnMozoDelDia_Click);
+            // 
+            // btnTotales
+            // 
+            this.btnTotales.Location = new System.Drawing.Point(44, 109);
+            this.btnTotales.Name = "btnTotales";
+            this.btnTotales.Size = new System.Drawing.Size(75, 34);
+            this.btnTotales.TabIndex = 3;
+            this.btnTotales.Text = "Totales";
+            this.btnTotales.UseVisualStyleBackColor = true;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(498, 388);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(78, 32);
+            this.btnSalir.TabIndex = 4;
+            this.btnSalir.Text = "&Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // mrcConsultas
+            // 
+            this.mrcConsultas.Controls.Add(this.lblResultado);
+            this.mrcConsultas.Controls.Add(this.lstResultado);
+            this.mrcConsultas.Controls.Add(this.btnMozoDelDia);
+            this.mrcConsultas.Controls.Add(this.btnTotales);
+            this.mrcConsultas.Enabled = false;
+            this.mrcConsultas.Location = new System.Drawing.Point(13, 185);
+            this.mrcConsultas.Name = "mrcConsultas";
+            this.mrcConsultas.Size = new System.Drawing.Size(451, 188);
+            this.mrcConsultas.TabIndex = 5;
+            this.mrcConsultas.TabStop = false;
+            this.mrcConsultas.Text = "Consultas";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(188, 29);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(55, 13);
+            this.lblResultado.TabIndex = 5;
+            this.lblResultado.Text = "Resultado";
+            // 
+            // lstResultado
+            // 
+            this.lstResultado.FormattingEnabled = true;
+            this.lstResultado.Location = new System.Drawing.Point(191, 45);
+            this.lstResultado.Name = "lstResultado";
+            this.lstResultado.Size = new System.Drawing.Size(200, 108);
+            this.lstResultado.TabIndex = 4;
+            // 
+            // lblVentas
+            // 
+            this.lblVentas.AutoSize = true;
+            this.lblVentas.Location = new System.Drawing.Point(10, 13);
+            this.lblVentas.Name = "lblVentas";
+            this.lblVentas.Size = new System.Drawing.Size(80, 13);
+            this.lblVentas.TabIndex = 6;
+            this.lblVentas.Text = "Grilla de ventas";
+            // 
+            // frmBarLaMilanga
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(596, 430);
+            this.Controls.Add(this.lblVentas);
+            this.Controls.Add(this.mrcConsultas);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnValidarDatos);
+            this.Controls.Add(this.dgvGrillaVentas);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmBarLaMilanga";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bar La Milanga";
+            this.Load += new System.EventHandler(this.frmBarLaMilanga_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvGrillaVentas)).EndInit();
+            this.mrcConsultas.ResumeLayout(false);
+            this.mrcConsultas.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvGrillaVentas;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.Button btnValidarDatos;
+        private System.Windows.Forms.Button btnMozoDelDia;
+        private System.Windows.Forms.Button btnTotales;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.GroupBox mrcConsultas;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.ListBox lstResultado;
+        private System.Windows.Forms.Label lblVentas;
+    }
+}
+
---
+++ pryHerreraSP4_BarLaMilanga/frmBarLaMilanga.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryHerreraSP4_BarLaMilanga
+{
+    public partial class frmBarLaMilanga : Form
+    {
+        public frmBarLaMilanga()
+        {
+            InitializeComponent();
+        }
+
+        float[,] Ventas = new float[5, 4];
+        public struct DATOS
+        {
+            public int mozos;
+            public int ventas;
+        }
+        
+        private void frmBarLaMilanga_Load(object sender, EventArgs e)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+              dgvGrillaVentas.Rows.Add();
+            }
+            dgvGrillaVentas.Rows[0].Cells[0].Value = "Julio";
+            dgvGrillaVentas.Rows[1].Cells[0].Value = "Esteban";
+            dgvGrillaVentas.Rows[2].Cells[0].Value = "Javier";
+            dgvGrillaVentas.Rows[3].Cells[0].Value = "Gonzalo";
+            dgvGrillaVentas.Rows[4].Cells[0].Value = "Alberto";
+            dgvGrillaVentas.Columns[0].ReadOnly = true;
+            dgvGrillaVentas.ClearSelection();
+        }
+
+        private void btnValidarDatos_Click(object sender, EventArgs e)
+        {
+            
+            for (int i = 0; i < dgvGrillaVentas.RowCount; i++)
+            {
+                if (dgvGrillaVentas.Rows[i].Cells[1].Value == null || dgvGrillaVentas.Rows[i].Cells[2].Value == null || 
+                    dgvGrillaVentas.Rows[i].Cells[3].Value == null || dgvGrillaVentas.Rows[i].Cells[4].Value == null)
+                {
+                    MessageBox.Show("Debe completar los datos de todas las ventas.",
+                                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+
+            int fallos = 0;
+
+            for (int columna = 1; columna < dgvGrillaVentas.ColumnCount; columna++)
+            {
+                for (int fila = 0; fila < dgvGrillaVentas.RowCount; fila++)
+                {
+                    string valor = dgvGrillaVentas.Rows[fila].Cells[columna].Value.ToString();
+                    bool a = float.TryParse(valor, out float result);
+
+                    if (a == false)
+                    {
+                        fallos++;
+                    }
+                }
+            }
+
+            if (fallos > 0)
+            {
+                mrcConsultas.Enabled = false;
+                MessageBox.Show("Ingrese únicamente valores númericos.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                fallos = 0;
+            }
+            else
+            {
+                for (int columna = 1; columna < dgvGrillaVentas.ColumnCount; columna++)
+                {
+                    for (int fila = 0; fila < dgvGrillaVentas.RowCount; fila++)
+                    {
+                        string dato = dgvGrillaVentas.Rows[fila].Cells[columna].Value.ToString();
+                        Ventas[fila, columna-1] = float.Parse(dato);
+                    }
+                }
+                mrcConsultas.Enabled = true;
+                MessageBox.Show("¡Datos ingresados con éxito!", "Notificación", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnMozoDelDia_Click(object sender, EventArgs e)
+        {
+            float Julio = 0;
+            for (int columna = 0; columna <= Ventas.GetUpperBound(1); columna++)
+            {
+                Julio += Ventas[0, columna];
+            }
+            
+
+            float Esteban = 0;
+            for (int columna = 0; columna <= Ventas.GetUpperBound(1); columna++)
+            {
+                Esteban += Ventas[1, columna];
+            }
+            
+
+            float Javier = 0;
+            for (int columna = 0; columna <= Ventas.GetUpperBound(1); columna++)
+            {
+                Javier += Ventas[2, columna];
+            }
+            
+
+            float Gonzalo = 0;
+            for (int columna = 0; columna <= Ventas.GetUpperBound(1); columna++)
+            {
+                Gonzalo += Ventas[3, columna];
+            }
+           
+
+            float Alberto = 0;
+            for (int columna = 0; columna <= Ventas.GetUpperBound(1); columna++)
+            {
+                Alberto += Ventas[4, columna];
+            }
+
+            float[] Mozos = new float[5] {Julio, Esteban, Javier, Gonzalo, Alberto};
+
+
+        }
+
+        private void dgvGrillaVentas_KeyPress(object sender, KeyPressEventArgs e)
+        {
+   
+        }
+
+        private void dgvGrillaVentas_Validating(object sender, CancelEventArgs e)
+        {
+    
+        }
+
+
+    }
+}
